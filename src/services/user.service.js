c6b431d84734c2a4f2c86f584ca6f492f7a6f2b3
Arafat <arafatahmedtanimcsedu57@gmail.com>
2021-02-21 19:12:31 +0600

    first commit

---
+++ src/services/user.service.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = 'http://localhost:8080/api/test/';
+
+class UserService {
+  getPublicContent() {
+    return axios.get(API_URL + 'all');
+  }
+
+  getUserContent() {
+    return axios.get(API_URL + 'user', { headers: authHeader() });
+  }
+
+  getModeratorContent() {
+    return axios.get(API_URL + 'mod', { headers: authHeader() });
+  }
+
+  getAdminContent() {
+    return axios.get(API_URL + 'admin', { headers: authHeader() });
+  }
+}
+
+export default new UserService();
